6bfb9aea8ef6d4ff1589dfcec6da3272f1e5154c
Ivan Avdeev <avdeev.1van@icloud.com>
2020-09-14 20:10:59 +0300

    Initial commit

---
+++ src/main/java/com/star_trello/darkside/DarkSideApplication.java
@@ -0,0 +1,13 @@
+package com.star_trello.darkside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/star_trello/darkside/DarkSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star_trello.darkside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
